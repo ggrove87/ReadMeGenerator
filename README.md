8eb5e842a06ebdb2004a0fa46a4b449444461e9a
ggrove87 <gwg1387@gmail.com>
2022-04-20 15:02:58 -0500

    Updated tags for sections

--- README.md
+++ README.md
@@ -1,32 +1,43 @@
 Fun Project
+  
+  Contact:gwg1387@gmail.com
+  
+  <br>
+  
+  Associated Links
+  
+  GIT Pages Link: 
+  
+  GIT Repository Link: 
+  
+  <br>
+  
+  ## <h2 id = "projectDescription"> Project Description </h2>
+  
+  * example text
+  <br>
+  
+  ## <h2 id = "tableOfContents"> Table of Contents: </h2>
+  <ol>
+  * <a href="#projectDescription">Description</a>
+  * <a href="#tableOfContents">Table of Contents</a>
+  * <a href="#installation">Installation</a>
+  * <a href="#usage">Usage</a>
+  * <a href="#license">License</a>
+  * <a href="#contributing">Contributing</a>
+  * <a href="#tests">Tests</a>
+  * <a href="#questions">Questions</a>
+  </ol>
+  
+  ## <h2 id = "installation">Installation </h2>
 
-Contact:gwg1387@gmail.com
+  ## <h2 id = "usage"> Usage </h2>
 
-<br>
+  ## <h2 id ="license"> License </h2>
 
-Associated Links
+  ## <h2 id="contributing"> Contributing </h2>
 
-GIT Pages Link: 
+  ## <h2 id="tests"> Tests </h2>
 
-GIT Repository Link: 
-
-<br>
-
-## Project Description
-
-* example text
-<br>
-
-## Built With:
-
-* HTML 5
-* CSS
-
-
-## Mock-Up
-
-The following image shows the web application's appearance and functionality:
-
-<br>
-
-![Image of the webpage being deployed](./assets/images/HW7_ScreenShot.png)
+  # <h2 id="questions">Questions</h2>
+  
--- index.js
+++ index.js
@@ -1,42 +1,79 @@
 const fs = require("fs");
+const inquirer = require("inquirer");
 let description = "example text";
 let projectName = "Fun Project";
 let contactEmail = "gwg1387@gmail.com";
 
-fs.writeFile(
-  "README.md",
-  `${projectName}
 
-Contact:${contactEmail}
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'list',
+      choices: ["MIT", "GPL",  ],
+      message: 'What license did you use?',
+      name: 'license',
+    },
+    {
+        type: 'input',
+        message: "What is you preferred method of communication?",
+        name: 'communicationMethod',
+    },
+  ])
+  .then((response) =>
+  fs.writeFile(
+    "README.md",
+    `${projectName}
+  
+  Contact:${contactEmail}
+  
+  <br>
+  
+  Associated Links
+  
+  GIT Pages Link: 
+  
+  GIT Repository Link: 
+  
+  <br>
+  
+  ## <h2 id = "projectDescription"> Project Description </h2>
+  
+  * ${description}
+  <br>
+  
+  ## <h2 id = "tableOfContents"> Table of Contents: </h2>
+  <ol>
+  * <a href="#projectDescription">Description</a>
+  * <a href="#tableOfContents">Table of Contents</a>
+  * <a href="#installation">Installation</a>
+  * <a href="#usage">Usage</a>
+  * <a href="#license">License</a>
+  * <a href="#contributing">Contributing</a>
+  * <a href="#tests">Tests</a>
+  * <a href="#questions">Questions</a>
+  </ol>
+  
+  ## <h2 id = "installation">Installation </h2>
 
-<br>
+  ## <h2 id = "usage"> Usage </h2>
 
-Associated Links
+  ## <h2 id ="license"> License </h2>
 
-GIT Pages Link: 
+  ## <h2 id="contributing"> Contributing </h2>
 
-GIT Repository Link: 
+  ## <h2 id="tests"> Tests </h2>
 
-<br>
-
-## Project Description
-
-* ${description}
-<br>
-
-## Built With:
-
-* HTML 5
-* CSS
+  # <h2 id="questions">Questions</h2>
+  `,
+    (err) => (err ? console.error(err) : console.log("Read me successfully generated!"))
+  )
+  );
 
 
-## Mock-Up
 
-The following image shows the web application's appearance and functionality:
 
-<br>
-
-![Image of the webpage being deployed](./assets/images/HW7_ScreenShot.png)
-`,
-  (err) => (err ? console.error(err) : console.log("Success!"))
-);
